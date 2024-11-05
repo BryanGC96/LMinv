558901eac105f2e641fdc997578ae1cf37b4f76e
BryanGC96 <2014104@icum.edu.mx>
2024-11-04 18:11:27 -0600

    Added Navbar

--- LmInvest/src/App.jsx
+++ LmInvest/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Navbar from './components/NavBar';
 import './App.css'
 
 function App() {
   return(
     <Router>
+      {/* La NavBar se mantiene aqui Permanentemente */}
+      <Navbar /> 
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
